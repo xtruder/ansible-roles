009efb33b8b31bf443b7a67724425c72faed0093
Jaka Hudoklin <jaka@x-truder.net>
2024-03-16 15:21:33 +0100

    roles/workstation-networking: rename to systemd-resolved

--- systemd-resolved/templates/dnsovertls.sh
+++ systemd-resolved/templates/dnsovertls.sh
@@ -4,7 +4,7 @@ COMMAND="$2"
 DEVICE="$1"
 
 # dns server to set
-DNS="1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com"
+DNS="{{ default_dns }}"
 
 if [[ "${COMMAND}" == "up" && ( "${DEVICE}" == wlp* || "${DEVICE}" == wifi* ) ]]; then
 	# temporary set dnsovertls to oppurtunistic mode until we are actually connected
